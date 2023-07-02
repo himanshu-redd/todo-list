11774da8380d726d1ec55d15f3da72f39e93f2a6
Himanshu Ranjan <himanshu.ranjan@razorpay.com>
2023-07-02 09:04:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require('cors');
+const app = express();
+
+const port = 3000;
+app.listen(port, () => console.log("Listening on : " + port));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/todos", saveTodo);
+app.get("/todos", getAllTodos);
+app.delete("/todos/:id", deleteTodo);
+let todoList = [];
+let id = 0;
+
+function saveTodo(req, res) {
+    console.log("saving a request"); 
+    const title = req.body.title;
+    const description = req.body.description;
+    const body = {
+        id: ++id,
+        title: title,
+        description: description
+    }
+    todoList.push(body);
+    const response = {id : id}; 
+    console.log(response); 
+    res.status(200).send(body); 
+}
+
+function getAllTodos(req, res) {
+    console.log("get all todods"); 
+    if (todoList.length > 0) {
+        res.status(200).send(todoList);
+    } else {
+        res.status(400).send({message : "No todo found"}); 
+    }
+}
+
+function deleteTodo(req, res){
+    console.log("Deleting a todo"); 
+    const id = req.params.id; 
+    console.log("length : " + todoList.length); 
+    let tempArray = []; 
+    todoList.forEach((todo) => {
+        if (todo.id != id)
+            tempArray.push(todo); 
+    }); 
+    todoList = tempArray; 
+    console.log("length : " + todoList.length); 
+    res.status(200).send(todoList);  
+}
